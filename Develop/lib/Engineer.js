8b8e537869c032457db5b0c6e4fb26631d7d2721
rpounceyjr <rpounceyjr@gmail.com>
2020-04-14 08:52:14 -0400

    engineer is now done.  everything works, but for some reason one of the tests won't pass.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, gitHubUserName) {
+        super(name, id, email);
+        this.gitHubUserName = gitHubUserName;
+    }
+    getRole() {
+        return "Engineer";
+    }
+    getGitHubUserName() {
+        return this.gitHubUserName;
+    }
+
+}
+
+const Roger = new Engineer('roger', 123, 'somethinghere', 'rpounceyjr');
+
+console.log(Roger.name);
+console.log(Roger.getGitHubUserName());
+console.log(Roger.id);
+console.log(Roger.email);
+
+module.exports = Engineer;
